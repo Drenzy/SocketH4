2f20efacbe971ccd4d3478497384cdd9edd44293
Daniel Hartwich <dhartwich650@gmail.com>
2024-02-26 09:12:24 +0100

    Add project files.

---
+++ SocketH4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace SocketH4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            IPEndPoint? ipEndPoint = GetIPEndPoint();
+            if (ipEndPoint != null) { new SocketServer(ipEndPoint);}
+            Console.WriteLine("No Ip EndPoint Found!!!");
+        }
+
+        static IPEndPoint? GetIPEndPoint()
+        {
+            IPHostEntry iPHostEntry = Dns.GetHostEntry(Dns.GetHostName(), AddressFamily.InterNetwork);
+            IPAddress[] addressList = iPHostEntry.AddressList;
+            //TODO pick IP Address more inteligently
+            if (addressList == null || addressList[0] == null) return null;
+            return new IPEndPoint(addressList[0], 8090);
+        }
+    }
+}
---
+++ SocketH4/SocketServer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketH4
+{
+    internal class SocketServer
+    {
+        private IPEndPoint IpEndPoint;
+
+        public SocketServer(IPEndPoint ipEndPoint)
+        {
+            this.IpEndPoint = ipEndPoint;
+        }
+
+        async Task Sever()
+        {
+            using Socket listener = new(
+            IpEndPoint.AddressFamily,
+            SocketType.Stream,
+            ProtocolType.Tcp);
+
+            listener.Bind(IpEndPoint);
+            listener.Listen(100);
+
+            var handler = await listener.AcceptAsync();
+            while (true)
+            {
+                // Receive message.
+                var buffer = new byte[1_024];
+                var received = await handler.ReceiveAsync(buffer, SocketFlags.None);
+                var response = Encoding.UTF8.GetString(buffer, 0, received);
+
+                var eom = "<|EOM|>";
+                if (response.IndexOf(eom) > -1 /* is end of message */)
+                {
+                    Console.WriteLine(
+                        $"Socket server received message: \"{response.Replace(eom, "")}\"");
+
+                    var ackMessage = "<|ACK|>";
+                    var echoBytes = Encoding.UTF8.GetBytes(ackMessage);
+                    await handler.SendAsync(echoBytes, 0);
+                    Console.WriteLine(
+                        $"Socket server sent acknowledgment: \"{ackMessage}\"");
+
+                    break;
+                }
+                // Sample output:
+                //    Socket server received message: "Hi friends 👋!"
+                //    Socket server sent acknowledgment: "<|ACK|>"
+            }
+        }
+    }
+}
